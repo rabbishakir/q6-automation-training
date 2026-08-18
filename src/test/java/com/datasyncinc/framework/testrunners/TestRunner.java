41e1d1ebe3e73a2afea007a63da26a7dbd01e8cc
rabbishakir <rabbishakir@gmail.com>
2026-08-18 11:48:09 -0400

    Back up Rabbi framework work

---
+++ src/test/java/com/datasyncinc/framework/testrunners/TestRunner.java
@@ -0,0 +1,23 @@
+package com.datasyncinc.framework.testrunners;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+		
+			features = "src/test/resources/features",
+			glue = "com.datasyncinc.framework.stepdefinations",
+			plugin = {"pretty", "html:target/cucumber-reports.html"},
+			tags = "@invalidLoginTest"
+		
+		)
+
+public class TestRunner {
+	
+	
+
+}
---
+++ src/test/java/com/dtasyncinc/framework/helper/BaseClass.java
@@ -0,0 +1,48 @@
+package com.dtasyncinc.framework.helper;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.datasyncinc.framework.utilities.ConfigReader;
+
+public class BaseClass {
+
+	static WebDriver driver;
+
+	// we will create a method
+	// that will give us the driver instance
+
+	public static WebDriver initializeDriver() throws IOException {
+
+		String browser = ConfigReader.getConfigAValue("browser");
+		int waitTime = Integer.parseInt(ConfigReader.getConfigAValue("implicitwait"));
+
+		if (browser.equals("Chrome")) {
+			driver = new ChromeDriver();
+		} else if (browser.equals("Firefox")) {
+			driver = new FirefoxDriver();
+		} else {
+			driver = new EdgeDriver();
+		}
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(waitTime));
+		driver.manage().window().maximize();
+		driver.get(ConfigReader.getConfigAValue("url"));
+		
+		return driver;
+	}
+
+	public static void quitDriver() {
+		// if the driver is not null
+		if (driver != null) {
+			driver.quit();
+		}
+
+	}
+
+}
